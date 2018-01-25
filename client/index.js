4224d9a9192ba6a111d792a0189a9f1fbfa92aa7
Justin <jvcruz@ucdavis.edu>
2018-01-25 00:02:26 -0800

    removed client js file and imported function to html file, also allowed enter to be used

--- client/index.js
+++
@@ -1,8 +0,0 @@
-
-const clickHandler = async function() {
-	const input = document.getElementById('input');
-	const result = document.getElementById('result');
-	const { data } = await axios.get(`/api/${input.value}`);
-	
-	result.innerHTML = data;
-};
