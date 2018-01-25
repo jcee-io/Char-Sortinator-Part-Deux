623c6b0209ca9564c5f2f249f58543254397f06b
Justin <jvcruz@ucdavis.edu>
2018-01-24 20:40:14 -0800

    server and test edits

--- server/index.js
+++ server/index.js
@@ -9,12 +9,11 @@ app.use(express.static('client'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('*', (req, res) => {
-	res.redirect('/');
-});
+
 
 app.get('/:string', (req, res) => {
-  res.send(req.params.string);
+	console.log(req.params);
+  res.send(req.params.string.split('').sort((a,b) => b > a).join(''));
 });
 app.listen(process.env.PORT || 3000, () => {
   console.log('app has started!');
--- server/test.js
+++ server/test.js
@@ -1,3 +1,15 @@
-test('initial test', () => {
-	expect(true).toBeTruthy();
+const axios = require('axios');
+
+
+describe('test1', async () => {
+
+
+	test('hello', async () => {
+		const data = await axios.get('http://localhost:3000/1a2b3c');
+		console.log(data);
+		expect(data).toEqual('cba321');
+	});
+
 });
+
+
